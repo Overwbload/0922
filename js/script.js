5dfc717c866603b633e54e150a73a4bb7e41ab46
Overwbload <134174390+Overwbload@users.noreply.github.com>
2023-09-22 07:12:11 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 300,
+        itemMargin: 20
+    });
+    
+    Fancybox.bind('[data-fancybox="gallery"]', {
+        
+      });
+
+});
